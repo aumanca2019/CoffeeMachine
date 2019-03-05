8863f6bf220fcd9d1d1f29978aba6fd058e3b7b2
aumanca2019 <ancalumi68@yahoo.com>
2019-03-06 00:16:43 +0200

    upload 0403

---
+++ coffeemachine/src/main/java/com/sda/java/cofeemachine/CoffeeType.java
@@ -0,0 +1,46 @@
+package com.sda.java.cofeemachine;
+
+public enum CoffeeType {
+    ESPRESSO(5,50,0,0),
+    LATTE(2,100,5,100),
+    FILTERCOFFEE(5,200,2,0);
+
+    private final int coffeeRequired;
+    private final int waterRequired;
+    private final int sugarRequired;
+    private final int milkRequired;
+
+    CoffeeType(int coffee, int water, int sugar, int milk) {
+        this.coffeeRequired = coffee;
+        this.waterRequired = water;
+        this.sugarRequired = sugar;
+        this.milkRequired = milk;
+    }
+
+    public int getCoffeeRequired() {
+        return coffeeRequired;
+    }
+
+    public int getWaterRequired() {
+        return waterRequired;
+    }
+
+    public int getSugarRequired() {
+        return sugarRequired;
+    }
+
+    public int getMilkRequired() {
+        return milkRequired;
+    }
+
+    @Override
+    public String toString() {
+        return "CoffeeType{" +
+                "coffeeRequired=" + coffeeRequired +
+                ", waterRequired=" + waterRequired +
+                ", sugarRequired=" + sugarRequired +
+                ", milkRequired=" + milkRequired +
+                '}';
+    }
+}
+
