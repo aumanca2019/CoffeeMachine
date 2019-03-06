f1417b77eebfa6b2e04d6452e904447425e3024c
aumanca2019 <ancalumi68@yahoo.com>
2019-03-06 20:36:10 +0200

    upload 0503

--- coffeemachine/src/main/java/com/sda/java/coffeemachine/Stock.java
+++ coffeemachine/src/main/java/com/sda/java/coffeemachine/Stock.java
@@ -1,4 +1,51 @@
 package com.sda.java.coffeemachine;
 
 public class Stock {
+
+
+    private int beansStock;
+    private int waterStock;
+    private int sugarStock;
+    private int milkStock;
+
+
+    public int getBeansStock() {
+        return beansStock;
+    }
+
+    public int getWaterStock() {
+        return waterStock;
+    }
+
+    public int getSugarStock() {
+        return sugarStock;
+    }
+
+    public int getMilkStock() {
+        return milkStock;
+    }
+
+    public void removeBeansFromStock(int quantity) {
+        beansStock -= quantity;
+
+    }
+
+    public void removeWaterFromStock(int quantity) {
+        waterStock -= quantity;
+    }
+
+    public void removeSugarFromStock(int quantity) {
+        sugarStock -= quantity;
+    }
+
+    public void removeMilkFromStock(int quantity) {
+        milkStock -= quantity;
+    }
+
+
+
+
 }
+
+
+
